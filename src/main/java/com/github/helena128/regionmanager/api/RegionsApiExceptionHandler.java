f8da53533122fd9efa8210493fd566788e1f22ce
Elena Tcheprasova <hellogbel@gmail.com>
2020-03-26 10:53:07 +0300

    Added logging

--- src/main/java/com/github/helena128/regionmanager/api/RegionsApiExceptionHandler.java
+++ src/main/java/com/github/helena128/regionmanager/api/RegionsApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.github.helena128.regionmanager.api;
 import com.github.helena128.regionmanager.execptions.RegionDoesNotExistException;
 import com.github.helena128.regionmanager.execptions.UniqueConstraintException;
 import io.swagger.model.ErrorModel;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +11,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Log4j2
 public class RegionsApiExceptionHandler {
 
     @ExceptionHandler(RegionDoesNotExistException.class)
     public ResponseEntity<ErrorModel> handleRegionDoesNotExist(RegionDoesNotExistException ex) {
+        log.error(ex.getMessage());
         val error = new ErrorModel().message(ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UniqueConstraintException.class)
     public ResponseEntity<ErrorModel> handleUniqueConstraint(UniqueConstraintException ex) {
+        log.error(ex.getMessage());
         val error = new ErrorModel().message(ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
--- src/main/java/com/github/helena128/regionmanager/service/RegionsServiceImpl.java
+++ src/main/java/com/github/helena128/regionmanager/service/RegionsServiceImpl.java
@@ -10,6 +10,7 @@ import io.swagger.model.RegionInput;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
 import static com.github.helena128.regionmanager.config.CacheConfig.REGIONS_CACHE_NAME;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RegionsServiceImpl implements RegionsService {
@@ -32,22 +34,24 @@ public class RegionsServiceImpl implements RegionsService {
 
     @Override
     public Region addRegion(RegionInput regionInput) {
-        //log.info("Called method to add region: " + regionInput.toString());
+        log.info("Called method to add region: {}", regionInput.toString());
         regionsValidator.validateRegionCreation(regionInput);
         val addedRegionEntityId = mybatisMapper.addRegionEntity(mapstructMapper.mapRegionInputToRegionEntity(regionInput));
-        //log.info("Added region, id: " + addedRegionEntity.getId());
+        log.info("Added region, id: {}", addedRegionEntityId);
         return this.findRegion(addedRegionEntityId.intValue());
     }
 
     @Override
     @CachePut(value = REGIONS_CACHE_NAME, key = "#id")
     public Region findRegion(Integer id) {
+        log.info("Called method to find region with id {}", id);
         regionsValidator.validateRegionExistence(id);
         return mapstructMapper.mapRegionionEntityToRegion(mybatisMapper.findRegionEntityById(id.longValue()));
     }
 
     @Override
     public OperationResultWithRegionList findRegions() {
+        log.info("Called method to find all regions");
         return this.findAllRegions()
                 .stream()
                 .map(mapstructMapper::mapRegionionEntityToRegion)
@@ -57,15 +61,17 @@ public class RegionsServiceImpl implements RegionsService {
     @Override
     @CacheEvict(value = REGIONS_CACHE_NAME, key = "#id")
     public void removeRegion(Integer id) {
+        log.info("Called method to remove region with id {}", id);
         regionsValidator.validateRegionExistence(id);
         mybatisMapper.removeById(id.longValue());
+        log.info("Successfully removed region with id {}", id);
     }
 
     @Override
-    @CachePut(value = REGIONS_CACHE_NAME, key = "#id")
     public Region updateRegion(Integer id, RegionInput regionInput) {
-        mybatisMapper.updateRegionById(id.longValue(), regionInput); // update
-        return mapstructMapper.mapRegionionEntityToRegion(mybatisMapper.findRegionEntityById(id.longValue()));
+        log.info("Called update method for region with id {}", id);
+        mybatisMapper.updateRegionById(id.longValue(), regionInput);
+        return this.findRegion(id);
     }
 
     @Cacheable(value = REGIONS_CACHE_NAME)
